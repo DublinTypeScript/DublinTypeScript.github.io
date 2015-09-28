e15879668021f6d949dc546d35f2b336ae3d67c0
Remo Jansen <remo.jansen@moneymate.com>
2015-09-28 16:02:47 +0100

    added slides

--- src/js/site.js
+++ src/js/site.js
@@ -86,6 +86,15 @@ window.requestAnimFrame = (function(){
     requestAnimFrame(animloop);
     skyline.render();
   })();
+  
+  // render previous meetups
+  $.get("./src/templates/previous_meetups.hbs", function(source) {
+    $.get("./data/previous_meetups.json", function(data) {
+      var template = Handlebars.compile(source);
+      var html = template(data);
+      $('#previous_meetups').html(html);
+    });
+  });
 
   // easter egg
   __konami__.enable(function() {
